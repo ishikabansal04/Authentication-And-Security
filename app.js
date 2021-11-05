ab9c8e76da3b8fa915ce393cdbac5f6ea1e9d77f
ishikabansal04 <ikabansal04@gmail.com>
2021-11-05 23:21:31 +0530

    Level 5 Security - Cookies and Sessions

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+
+// ******************************* LEVEL 1 & LEVEL 2 Security - Login-Registration & Encryption ***************************** //
+
 //jshint esversion:6
+/*
 require('dotenv').config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
-const bcrypt = require("bcrypt");
-
-const saltRounds = 10;
+const encrypt = require("mongoose-encryption"); 
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -17,14 +17,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-
-// ******************************* LEVEL 2 Security - Encryption ***************************** //
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]}); // used for LEVEL 2 security 
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]}); // used for LEVEL 2 security 
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,13 +39,95 @@ app.get("/register", function(req, res){
 });
 
 
-
-// ******************************* LEVEL 1 Security - Registration & Login ***************************** //
-/*
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: md5(req.body.password) // ******************************* LEVEL 3 Security - Hashing (md5 hash function) ***************************** //
+        password: req.body.password 
+    });
+    newUser.save(function(err){
+        if(!err){
+            res.render("secrets");
+        }
+        else{
+            console.log(err);
+        }
+    });
+});
+
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+}); 
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
+*/
+
+
+
+
+
+
+
+// ******************************* LEVEL 3 Security - Hashing (md5 hash function) ***************************** //
+
+//jshint esversion:6
+/*
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const md5 = require("md5"); 
+const saltRounds = 10;
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: md5(req.body.password) 
     });
     newUser.save(function(err){
         if(!err){
@@ -77,12 +157,60 @@ app.post("/login", function(req, res){
         }
     });
 }); 
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
 */
 
 
 
 
+
+
+
+
+
 // ******************************* LEVEL 4 Security - Salting And Hashing (bcrypt hash function) ***************************** //
+
+//jshint esversion:6
+/*
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
 app.post("/register", function(req, res){
 
     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
@@ -125,9 +253,123 @@ app.post("/login", function(req, res){
 }); 
 
 
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
+*/
+
+
+
+
+
+
+
+
+
+
+
+// ******************************* LEVEL 5 Security - passport (Cookies & Sessions) ***************************** //
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+}); 
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }
+    else{
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+app.post("/register", function(req, res){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
+
+app.post("/login", function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+            // res.redirect("/login");
+        }
+        else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+}); 
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
-
-
